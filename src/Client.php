afbed7796208567beebd4d5586349a6a16b068b1
Kieran Brahney <kieran@supportpal.com>
2021-09-20 16:55:10 +0100

    Set Content-Type header to application/json

--- src/Client.php
+++ src/Client.php
@@ -472,7 +472,7 @@ class Client
             throw new RuntimeException(sprintf('JSON encoding error %s: %s', json_last_error(), json_last_error_msg()));
         }
 
-        $this->guzzle->post($this->endpoint, ['body' => $encoded]);
+        $this->guzzle->post($this->endpoint, ['json' => $encoded]);
     }
 
     /**
